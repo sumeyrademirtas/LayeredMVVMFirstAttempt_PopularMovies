c0ad43a56ccbcde8d27b0e4e99a4615a2a6db2e1
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-01-03 12:56:28 +0300

    Initial Commit

---
+++ LayeredMVVMFirstAttempt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LayeredMVVMFirstAttempt
+//
+//  Created by Sümeyra Demirtaş on 1/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
