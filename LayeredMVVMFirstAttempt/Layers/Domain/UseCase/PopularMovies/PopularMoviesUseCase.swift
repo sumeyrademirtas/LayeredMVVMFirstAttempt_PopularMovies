c4efbd3c1b1c43e857b302bda98e441ed1758e6e
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-01-15 18:13:03 +0300

    refactor(usecase): updated to Combine-based structure with enhanced comments
    # Replaced completion handlers with Combine's Publisher for fetchPopularMovies.
    # Improved comments for better clarity and understanding.

--- LayeredMVVMFirstAttempt/Layers/Domain/UseCase/PopularMovies/PopularMoviesUseCase.swift
+++ LayeredMVVMFirstAttempt/Layers/Domain/UseCase/PopularMovies/PopularMoviesUseCase.swift
@@ -6,21 +6,27 @@
 //
 
 import Foundation
+import Combine
 
+// MARK: - Protocol Definition
 protocol PopularMoviesUseCase {
-    func fetchPopularMovies(page: Int, completion: @escaping (Result<[Movie], Error>) -> Void)
+    func fetchPopularMovies(page: Int) -> AnyPublisher<[Movie], Error>
 }
 
+// MARK: - Implementation
 struct PopularMoviesUseCaseImplementation: PopularMoviesUseCase {
     private let service: PopularMoviesService
 
+    /// Dependency Injection
     init(service: PopularMoviesService) {
-        self.service = service  // bu atama/tanim sayesinde struct icindeki fonksiyonlar service i  kullanabilecek.
+        self.service = service  /// This definition allows the function within the struct to utilize the service.
     }
 }
 
+// MARK: - Combine Compatible Fetch
 extension PopularMoviesUseCaseImplementation {
-    func fetchPopularMovies(page: Int, completion: @escaping (Result<[Movie], Error>) -> Void) {
-        service.fetchPopularMovies(page: page, completion: completion)
+    func fetchPopularMovies(page: Int) -> AnyPublisher<[Movie], Error> {
+        /// Calling the fetch method from the service layer and returning a Publisher
+        service.fetchPopularMovies(page: page)
     }
 }
