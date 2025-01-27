87ed4f509bcd06691268d5e5d172fa0945221a57
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-01-27 21:46:03 +0300

    feat(model): implemented dynamic movie fetching and categories
    - Added `Movie` and `MoviesResponse` structs to model movie data.
    - Introduced `MovieCategory` enum for dynamic API endpoint management and display name mapping.
    - Enhanced `fullPosterURL` in `Movie` for image fetching.

---
+++ LayeredMVVMFirstAttempt/Layers/Domain/Remotes/Movies/Models/Movie.swift
@@ -0,0 +1,69 @@
+//
+//  Movie.swift
+//  LayeredMVVMFirstAttempt
+//
+//  Created by Sümeyra Demirtaş on 1/15/25.
+//
+
+import Foundation
+
+// MARK: - Movie
+struct Movie: Decodable {
+    let id: Int
+    let title: String
+    let overview: String
+    let posterPath: String
+    let releaseDate: String
+    let voteAverage: Double
+    
+    // Tam URL oluşturmak için bir computed property
+    var fullPosterURL: String {
+        let baseURL = "https://image.tmdb.org/t/p/w500" // Resim için temel URL
+        return "\(baseURL)\(posterPath)"
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case id, title, overview
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case voteAverage = "vote_average"
+    }
+}
+
+// MARK: - MoviesResponse
+struct MoviesResponse: Decodable {
+    let results: [Movie]
+}
+
+enum MovieCategory: String {
+    case nowPlaying = "now_playing"
+    case popular = "popular"
+    case topRated = "top_rated"
+    case upcoming = "upcoming"
+    
+    var endpoint: String {
+            switch self {
+            case .nowPlaying:
+                return "/movie/now_playing"
+            case .popular:
+                return "/movie/popular"
+            case .topRated:
+                return "/movie/top_rated"
+            case .upcoming:
+                return "/movie/upcoming"
+            }
+        }
+    
+    var displayName: String {
+            switch self {
+            case .nowPlaying:
+                return "Now Playing"
+            case .popular:
+                return "Popular Movies"
+            case .topRated:
+                return "Top Rated"
+            case .upcoming:
+                return "Upcoming"
+            }
+        }
+}
