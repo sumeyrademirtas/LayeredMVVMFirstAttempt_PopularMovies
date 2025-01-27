23b343bc7662338a377107426df16f5956b65ab0
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-01-27 22:03:13 +0300

    refactor(builder): updated PopularMoviesBuilder to support dynamic movie categories
    # Replaced specific service, use case, and view model implementations with generic ones.
    # Switched PopularMoviesTableViewProvider to MoviesCollectionViewProvider for dynamic rendering.
    # Removed hardcoded category dependencies in the builder.

--- LayeredMVVMFirstAttempt/Builder/PopularMovies/PopularMoviesBuilder.swift
+++ LayeredMVVMFirstAttempt/Builder/PopularMovies/PopularMoviesBuilder.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Moya
 
 protocol PopularMoviesBuilder {
     func build() -> UIViewController
@@ -18,13 +19,15 @@ struct PopularMoviesBuilderImplementation: PopularMoviesBuilder {
         
         let constants = Constants()
         
-        let service = PopularMoviesServiceImplementation(constants: constants)
+//        let service = PopularMoviesServiceImplementation(constants: constants)
+        let service = TmdbServiceImpl()
+
         
-        let useCase = PopularMoviesUseCaseImplementation(service: service)
+        let useCase = MoviesUseCaseImplementation(service: service)
         
-        let viewModel = PopularMoviesViewModel(useCase: useCase)
+        let viewModel = MoviesViewModel(useCase: useCase)
                 
-        let provider = PopularMoviesTableViewProviderImpl()
+        let provider = MoviesCollectionViewProviderImpl()
         
         let vc = PopularMoviesViewController(viewModel: viewModel, provider: provider)
         
