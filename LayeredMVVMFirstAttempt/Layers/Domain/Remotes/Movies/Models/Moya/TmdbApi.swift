75c304f7803be870b52561b466d47af921c7fe98
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-01-29 11:01:03 +0300

    refactor(TmdbApi): revert to explicit API cases for movie categories
    # Removed the dynamic `getMovies(category: MovieCategory, page: Int)` case
    # Reintroduced explicit API cases: `getPopularMovies`, `getTopRatedMovies`, `getNowPlayingMovies`, and `getUpcomingMovies`
    # Adjusted endpoint mapping and request parameters accordingly
    # Improved clarity and maintainability by defining separate cases for each movie category

--- LayeredMVVMFirstAttempt/Layers/Domain/Remotes/Movies/Models/Moya/TmdbApi.swift
+++ LayeredMVVMFirstAttempt/Layers/Domain/Remotes/Movies/Models/Moya/TmdbApi.swift
@@ -8,11 +8,10 @@ import Foundation
 import Moya
 
 enum TMDBApi {
-//    case getPopularMovies(page: Int)
-//    case getTopRatedMovies(page: Int)
-//    case getNowPlayingMovies(page: Int)
-//    case getUpcomingMovies(page: Int)
-    case getMovies(category: MovieCategory, page: Int) // Dinamik olarak kategori ve sayfa alır
+    case getPopularMovies(page: Int)
+    case getTopRatedMovies(page: Int)
+    case getNowPlayingMovies(page: Int)
+    case getUpcomingMovies(page: Int)    
 }
 
 extension TMDBApi: TargetType {
@@ -26,8 +25,14 @@ extension TMDBApi: TargetType {
     
     var path: String {
         switch self {
-        case .getMovies(let category, _):
-            return category.endpoint // MovieCategory'deki endpoint'i kullanır
+        case .getPopularMovies:
+            return "/movie/popular"
+        case .getTopRatedMovies:
+            return "/movie/top_rated"
+        case .getNowPlayingMovies:
+            return "/movie/now_playing"
+        case .getUpcomingMovies:
+            return "/movie/upcoming"
         }
     }
     
@@ -36,14 +41,15 @@ extension TMDBApi: TargetType {
     }
     
     var task: Task {
-         switch self {
-         case .getMovies(_, let page):
-             return .requestParameters(parameters: [
-                 "page": page,
-                 "api_key": constants.apiKey,
-                 "language": "en-US"
-             ], encoding: URLEncoding.default)
-         }
+
+        switch self {
+        case .getPopularMovies(page: let page), .getTopRatedMovies(page: let page), .getNowPlayingMovies(page: let page), .getUpcomingMovies(page: let page):
+            var params = [String:Any]()
+            params["api_key"] = constants.apiKey
+            params["page"] = page
+            params["language"] = "en-US"
+            return .requestParameters(parameters: params, encoding: URLEncoding.default)
+        }
      }
     
     var headers: [String: String]? {
